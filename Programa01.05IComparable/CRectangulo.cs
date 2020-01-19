b51615db57bb444f1346326fd869a495a83ad60c
JuanBustosV <juanbv81@gmail.com>
2020-01-19 10:46:02 +0100

    Ejemplo Icomparable

---
+++ Programa01.05IComparable/CRectangulo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa01._05IComparable
+{
+    class CRectangulo : IComparable
+    {
+        // Propiedades
+        public double Ancho { get; set; }
+        public double Alto { get; set; }
+        public double Area { get; set; }
+
+        // Constructor
+
+        public CRectangulo(double pAncho, double pAlto)
+        {
+            Ancho = pAncho;
+            Alto = pAlto;
+            CalculaArea();
+        }
+
+        private void CalculaArea()
+        {
+            Area = Ancho * Alto;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("[{0},{1}]={2}", Ancho, Alto, Area);
+        }
+
+        // Implementación de IComparable
+
+        int IComparable.CompareTo(object obj)
+        {
+            // Hacemos type cast con el objeto con el cual nos vamos a comparar
+            CRectangulo temp = (CRectangulo)obj;
+
+            // Si somos más grandes que el objeto devolvemos 1
+            if (Area > temp.Area)
+                return 1;
+
+            // Si somos más pequeños que el objeto devolvemos -1
+            if (Area < temp.Area)
+                return -1;
+
+            // Si somos iguales al objeto devuelve 0
+            return 0;
+        }
+    }
+}
---
+++ Programa01.05IComparable/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa01._05IComparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Tutorial C# nivel intermedio 6 --- IComparable";
+
+            CRectangulo[] rects = new CRectangulo[5];
+
+            rects[0] = new CRectangulo(7, 5);
+            rects[1] = new CRectangulo(6, 4);
+            rects[2] = new CRectangulo(4, 3);
+            rects[3] = new CRectangulo(7, 6);
+            rects[4] = new CRectangulo(5, 7);
+
+            // Imprimimos array original
+            foreach (CRectangulo r in rects)
+                Console.WriteLine(r);
+
+            Console.WriteLine("-------------------------");
+
+            // Hacemos uso de sort, el cual necesita la implementación de IComparable
+            Array.Sort(rects);
+
+            // Imprimimos arreglo ordenado
+            foreach (CRectangulo r in rects)
+                Console.WriteLine(r);
+        }
+    }
+}
